f07485a53fe2a410ffc36125bf742a3190a6397e
Zilvinas Milvydas <91484577+30028170@users.noreply.github.com>
2022-05-31 14:23:25 +0100

    Auto Clicker
    Probably has some bugs, but it does autoclick so I guess it works.

---
+++ Main.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace AutoClicker
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.siticoneTrackBar1 = new Siticone.Desktop.UI.WinForms.SiticoneTrackBar();
+            this.MinCpsLabel = new System.Windows.Forms.Label();
+            this.MaxCpsLabel = new System.Windows.Forms.Label();
+            this.siticoneTrackBar2 = new Siticone.Desktop.UI.WinForms.SiticoneTrackBar();
+            this.MinCpsValue = new System.Windows.Forms.Label();
+            this.MaxCpsValue = new System.Windows.Forms.Label();
+            this.Closedown = new System.Windows.Forms.Label();
+            this.Minimize = new System.Windows.Forms.Label();
+            this.Title = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Info = new System.Windows.Forms.Label();
+            this.StartButton = new Siticone.Desktop.UI.WinForms.SiticoneGradientButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.range = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // siticoneTrackBar1
+            // 
+            this.siticoneTrackBar1.HoverState.Parent = this.siticoneTrackBar1;
+            this.siticoneTrackBar1.Location = new System.Drawing.Point(12, 64);
+            this.siticoneTrackBar1.Name = "siticoneTrackBar1";
+            this.siticoneTrackBar1.Size = new System.Drawing.Size(300, 23);
+            this.siticoneTrackBar1.TabIndex = 0;
+            this.siticoneTrackBar1.ThumbColor = System.Drawing.Color.White;
+            this.siticoneTrackBar1.Value = 10;
+            this.siticoneTrackBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.siticoneTrackBar1_Scroll);
+            // 
+            // MinCpsLabel
+            // 
+            this.MinCpsLabel.AutoSize = true;
+            this.MinCpsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinCpsLabel.ForeColor = System.Drawing.Color.White;
+            this.MinCpsLabel.Location = new System.Drawing.Point(8, 41);
+            this.MinCpsLabel.Name = "MinCpsLabel";
+            this.MinCpsLabel.Size = new System.Drawing.Size(108, 20);
+            this.MinCpsLabel.TabIndex = 1;
+            this.MinCpsLabel.Text = "Minimum CPS";
+            // 
+            // MaxCpsLabel
+            // 
+            this.MaxCpsLabel.AutoSize = true;
+            this.MaxCpsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaxCpsLabel.ForeColor = System.Drawing.Color.White;
+            this.MaxCpsLabel.Location = new System.Drawing.Point(8, 91);
+            this.MaxCpsLabel.Name = "MaxCpsLabel";
+            this.MaxCpsLabel.Size = new System.Drawing.Size(112, 20);
+            this.MaxCpsLabel.TabIndex = 3;
+            this.MaxCpsLabel.Text = "Maximum CPS";
+            // 
+            // siticoneTrackBar2
+            // 
+            this.siticoneTrackBar2.HoverState.Parent = this.siticoneTrackBar2;
+            this.siticoneTrackBar2.Location = new System.Drawing.Point(12, 114);
+            this.siticoneTrackBar2.Name = "siticoneTrackBar2";
+            this.siticoneTrackBar2.Size = new System.Drawing.Size(300, 23);
+            this.siticoneTrackBar2.TabIndex = 2;
+            this.siticoneTrackBar2.ThumbColor = System.Drawing.Color.White;
+            this.siticoneTrackBar2.Value = 20;
+            this.siticoneTrackBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.siticoneTrackBar2_Scroll);
+            // 
+            // MinCpsValue
+            // 
+            this.MinCpsValue.AutoSize = true;
+            this.MinCpsValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinCpsValue.ForeColor = System.Drawing.Color.White;
+            this.MinCpsValue.Location = new System.Drawing.Point(318, 64);
+            this.MinCpsValue.Name = "MinCpsValue";
+            this.MinCpsValue.Size = new System.Drawing.Size(27, 20);
+            this.MinCpsValue.TabIndex = 4;
+            this.MinCpsValue.Text = "10";
+            this.MinCpsValue.Click += new System.EventHandler(this.MinCpsValue_Click);
+            // 
+            // MaxCpsValue
+            // 
+            this.MaxCpsValue.AutoSize = true;
+            this.MaxCpsValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaxCpsValue.ForeColor = System.Drawing.Color.White;
+            this.MaxCpsValue.Location = new System.Drawing.Point(318, 114);
+            this.MaxCpsValue.Name = "MaxCpsValue";
+            this.MaxCpsValue.Size = new System.Drawing.Size(27, 20);
+            this.MaxCpsValue.TabIndex = 5;
+            this.MaxCpsValue.Text = "20";
+            this.MaxCpsValue.Click += new System.EventHandler(this.MaxCpsValue_Click);
+            // 
+            // Closedown
+            // 
+            this.Closedown.AutoSize = true;
+            this.Closedown.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Closedown.ForeColor = System.Drawing.Color.White;
+            this.Closedown.Location = new System.Drawing.Point(333, 10);
+            this.Closedown.Name = "Closedown";
+            this.Closedown.Size = new System.Drawing.Size(14, 13);
+            this.Closedown.TabIndex = 6;
+            this.Closedown.Text = "X";
+            this.Closedown.Click += new System.EventHandler(this.Closedown_Click);
+            // 
+            // Minimize
+            // 
+            this.Minimize.AutoSize = true;
+            this.Minimize.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Minimize.ForeColor = System.Drawing.Color.White;
+            this.Minimize.Location = new System.Drawing.Point(314, 7);
+            this.Minimize.Name = "Minimize";
+            this.Minimize.Size = new System.Drawing.Size(13, 13);
+            this.Minimize.TabIndex = 7;
+            this.Minimize.Text = "_";
+            this.Minimize.Click += new System.EventHandler(this.Minimize_Click);
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.White;
+            this.Title.Location = new System.Drawing.Point(11, 7);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(61, 13);
+            this.Title.TabIndex = 8;
+            this.Title.Text = "AutoClicker";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.Info);
+            this.panel1.Controls.Add(this.Closedown);
+            this.panel1.Controls.Add(this.Minimize);
+            this.panel1.Controls.Add(this.Title);
+            this.panel1.Location = new System.Drawing.Point(-2, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(358, 28);
+            this.panel1.TabIndex = 9;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Main_MouseDown);
+            // 
+            // Info
+            // 
+            this.Info.AutoSize = true;
+            this.Info.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Info.ForeColor = System.Drawing.Color.White;
+            this.Info.Location = new System.Drawing.Point(255, 7);
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(53, 13);
+            this.Info.TabIndex = 9;
+            this.Info.Text = "F9 = Stop";
+            // 
+            // StartButton
+            // 
+            this.StartButton.CheckedState.Parent = this.StartButton;
+            this.StartButton.CustomImages.Parent = this.StartButton;
+            this.StartButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.StartButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.StartButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.StartButton.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.StartButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.StartButton.DisabledState.Parent = this.StartButton;
+            this.StartButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.StartButton.ForeColor = System.Drawing.Color.White;
+            this.StartButton.HoverState.Parent = this.StartButton;
+            this.StartButton.Location = new System.Drawing.Point(12, 182);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.ShadowDecoration.Parent = this.StartButton;
+            this.StartButton.Size = new System.Drawing.Size(333, 45);
+            this.StartButton.TabIndex = 11;
+            this.StartButton.Text = "START";
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // range
+            // 
+            this.range.AutoSize = true;
+            this.range.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.range.ForeColor = System.Drawing.Color.White;
+            this.range.Location = new System.Drawing.Point(12, 150);
+            this.range.Name = "range";
+            this.range.Size = new System.Drawing.Size(93, 20);
+            this.range.TabIndex = 12;
+            this.range.Text = "Range CPS";
+            this.range.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.ClientSize = new System.Drawing.Size(354, 236);
+            this.Controls.Add(this.range);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.MaxCpsValue);
+            this.Controls.Add(this.MinCpsValue);
+            this.Controls.Add(this.MaxCpsLabel);
+            this.Controls.Add(this.siticoneTrackBar2);
+            this.Controls.Add(this.MinCpsLabel);
+            this.Controls.Add(this.siticoneTrackBar1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoClicker";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Siticone.Desktop.UI.WinForms.SiticoneTrackBar siticoneTrackBar1;
+        private System.Windows.Forms.Label MinCpsLabel;
+        private System.Windows.Forms.Label MaxCpsLabel;
+        private Siticone.Desktop.UI.WinForms.SiticoneTrackBar siticoneTrackBar2;
+        private System.Windows.Forms.Label MinCpsValue;
+        private System.Windows.Forms.Label MaxCpsValue;
+        private System.Windows.Forms.Label Closedown;
+        private System.Windows.Forms.Label Minimize;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Info;
+        private Siticone.Desktop.UI.WinForms.SiticoneGradientButton StartButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label range;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AutoClicker
+{
+    public partial class Main : Form
+    {
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        public const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        public const int MOUSEEVENTF_LEFTUP = 0x0004;
+        public int min = 10;
+        public int max = 20;
+        public int random = 0;
+
+        public Main()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+            this.KeyUp += new KeyEventHandler(KeyEvent);
+        }
+
+        private void KeyEvent(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F9)
+            {
+                timer1.Enabled = false;
+                StartButton.Text = "START";
+            }
+        }
+
+        private void MouseClickLeft()
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            Thread.Sleep(80);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        }
+
+        private int ClickRange(int minCPS, int maxCPS)
+        {
+            Random r = new Random();
+            int rangeInt = r.Next(minCPS, maxCPS+1);
+            return rangeInt;
+        }
+
+        private void StartClicking(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MinCpsValue_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MaxCpsValue_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneTrackBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            if (siticoneTrackBar1.Value == 0)
+            {
+                siticoneTrackBar1.Value = 1;
+            }
+            else if (siticoneTrackBar1.Value > siticoneTrackBar2.Value)
+            {
+                siticoneTrackBar1.Value = siticoneTrackBar2.Value - 1;
+            }
+            else if (siticoneTrackBar1.Value == siticoneTrackBar2.Value)
+            {
+                siticoneTrackBar1.Value = siticoneTrackBar2.Value - 1;
+            }
+
+            min = siticoneTrackBar1.Value;
+
+            MinCpsValue.Text = siticoneTrackBar1.Value.ToString();
+        }
+
+        private void siticoneTrackBar2_Scroll(object sender, ScrollEventArgs e)
+        {
+            if (siticoneTrackBar2.Value < siticoneTrackBar1.Value)
+            {
+                siticoneTrackBar2.Value = siticoneTrackBar1.Value + 1;
+            }
+            else if (siticoneTrackBar2.Value == siticoneTrackBar1.Value)
+            {
+                siticoneTrackBar2.Value = siticoneTrackBar1.Value + 1;
+            }
+
+            max = siticoneTrackBar2.Value;
+
+            MaxCpsValue.Text = siticoneTrackBar2.Value.ToString();
+        }
+
+        private void Closedown_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Minimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Main_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (StartButton.Text == "START")
+            {
+                timer1.Enabled = true;
+                StartButton.Text = "STOP";
+            }
+            else
+            {
+                timer1.Enabled = false;
+                StartButton.Text = "START";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            random = ClickRange(min, max);
+            range.Text = random.ToString();
+
+            for (int i = 0; i < random; i++)
+            {
+                MouseClickLeft();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
